c463443495072a5ed903a404a114ed7f4be833fc
Mario Afonso TI <profissional@marioafonso.com>
2021-11-05 00:55:17 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,34 @@
 import NavBar from 'components';
+import BarChart from 'components/BarChart';
 import DataTable from 'components/DataTable';
+import DonutChart from 'components/DonutChart';
 import Footer from 'components/Footer';
 import React from 'react';
 
 function App() {
   return (
     <><NavBar />
-    <div className="container">
-      <h1 className="text-primary">Olá Mundo!</h1>
+      <div className="container">
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
 
-      <DataTable />
-    </div>
-    <Footer /></>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Sucesso das Vendas (%)</h5>
+            <BarChart />
+          </div>
+
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Todas as Vendas</h5>
+            <DonutChart />
+          </div>
+        </div>
+
+        <div className="row px-3">
+          <h2 className="text-primary">Todas as Vendas</h2>
+        </div>
+        <DataTable />
+      </div>
+      <Footer /></>
   );
 }
 
