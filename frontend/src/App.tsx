2fb226074f1a9bc921c75b4829a1aca2f1a8ed4a
Mario Afonso TI <profissional@marioafonso.com>
2021-11-05 00:37:59 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
+import NavBar from 'components';
+import DataTable from 'components/DataTable';
+import Footer from 'components/Footer';
 import React from 'react';
 
 function App() {
   return (
-    <div >
+    <><NavBar />
+    <div className="container">
       <h1 className="text-primary">Olá Mundo!</h1>
+
+      <DataTable />
     </div>
+    <Footer /></>
   );
 }
 
